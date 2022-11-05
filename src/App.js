25bcee85288438a4dc788f20652ca71c104a25db
Janet <janetwong024@gmail.com>
2022-11-05 17:29:44 -0400

    created engine component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Engine from "./Engine";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <Engine />
     </div>
   );
 }
