73779d9ffcdf416837fe578c1c4aab71c5321130
Janet <janetwong024@gmail.com>
2022-11-05 17:42:57 -0400

    created weather component and added css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import "./App.css";
 import Engine from "./Engine";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <Engine />
+      <div className="app-container">
+        <Engine />
+        <Weather />
+      </div>
+      <footer>
+        {" "}
+        This project was coded by Janet Wong and is {""}
+        <a
+          href="https://github.com/jwong02/react-weather-application"
+          target="_blank"
+        >
+          open-sourced on GitHub.
+        </a>
+      </footer>
     </div>
   );
 }
